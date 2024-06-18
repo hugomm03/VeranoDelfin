276768c6a37dad6bba8f03f6f8712aeefe0d0edf
hugomm03 <hugoarmandomm03@gmail.com>
2024-06-18 15:13:47 -0600

    Updated

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenidos a la Aplicación Del Cerebro</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  titulo: {
+    fontSize: 30,
+  }
+});
