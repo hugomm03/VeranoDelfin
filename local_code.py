783eb9f61530161ed4ba357430aa07f700e734ab
hugomm03 <hugoarmandomm03@gmail.com>
2024-06-24 16:30:15 -0600

    comit encima del primer commit remoto

--- local_code.py
+++
@@ -1,8 +0,0 @@
-import paho.mqtt.publish as publish
-
-mqtt_broker_address = "192.168.0.107"
-mqtt_channel = "your/command/channel"
-
-message = "On"
-
-publish.single(mqtt_channel,message,hostname=mqtt_broker_address)
