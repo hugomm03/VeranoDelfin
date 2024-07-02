f3f5b2fb22746543f037760006223678642342f3
hugomm03 <hugoarmandomm03@gmail.com>
2024-07-01 20:55:24 -0600

    y

--- Aplicacion/Habla.js
+++ Aplicacion/Habla.js
@@ -8,7 +8,27 @@ import ElHabla from './assets/ElHabla.mp4';
 
 export const Habla = ({navigation}) => {
     const video = useRef(null);
-    const [status,setStatus]= useState({});
+    const [state,setState]= useState({hasAutoplayed: false,playing: true});
+
+
+	onStatusUpdate = status => {
+		const isPlaying = status.isLoaded && status.isPlaying;
+
+		if ( ! state.hasAutoplayed ) {
+			// Wait until playback starts before synching.
+			if ( ! isPlaying ) {
+				return;
+			}
+
+			setState( { hasAutoplayed: true } );
+		}
+
+		if ( state.playing !== isPlaying ) {
+			setState( { playing: isPlaying } );
+		}
+	}
+
+
     return(
         <View style={styles.container}>
             <Video
@@ -18,10 +38,12 @@ export const Habla = ({navigation}) => {
                 useNativeControls
                 resizeMode={ResizeMode.COVER}
                 isLooping
-                onPlaybackStatusUpdate={status => setStatus(() => status)}
+                shouldPlay={ state.playing }
+			    onPlaybackStatusUpdate={onStatusUpdate}
             />
-
-
+            <View style={{flex:1}}>
+                <Text style={styles.text}>ÁREA DEL HABLA</Text>
+            </View>
         </View>
     );
 }
@@ -30,10 +52,24 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    text:{ 
+        fontSize: 50,
+        color: '#fff',
+        marginTop: 0,
+         //bottom: 250,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+
     video:{
+        position: 'absolute',
+        top:0,
+        left:0,
+        bottom:0,
+        right:0,
         //alignSelf: 'center',
         //margin: 16,
-        width: 384,
-        height: 756,
+        //width: '100%',
+        //height: '100%',
     }
   });
